58dffa1b18cbf886a4d3dd7ab6325bc8e0ecddff
SunValley <buckovika0@gmail.com>
2024-07-08 18:55:24 +0300

    .

--- StringsAndStructures/Program.cs
+++ StringsAndStructures/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using static System.Net.Mime.MediaTypeNames;
-
-namespace StringsAndStructures
+﻿namespace StringsAndStructures
 {   
     internal class Program
     {
